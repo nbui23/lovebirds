cf6b183d5ddaa729b1e0765461a167428acd2833
Norman Bui <normanbui23@gmail.com>
2023-09-21 04:36:08 -0400

    Initial Commit

---
+++ Lovebirds/LovebirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovebirdsApp.swift
+//  Lovebirds
+//
+//  Created by Norman Bui on 2023-09-21.
+//
+
+import SwiftUI
+
+@main
+struct LovebirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
